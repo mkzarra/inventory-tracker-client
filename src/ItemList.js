0cf6f6ccfcbc28a2e9a251663f7d060a059c0a9b
mkzarra <mkzarra@gmail.com>
2018-10-02 11:32:52 -0400

    completed ItemList.js

---
+++ src/ItemList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Item from './Item.js'
+import './ItemList.css'
+
+const ItemList = props => {
+  // props = the item list taken from <Main /> state + itemID setter method, token, axios GET method
+
+  const Items = props.items.map((itemData, index) => {
+    return (
+      <Item setCurrentFormItemID={props.setCurrentFormItemID} token={props.token} itemRequest={props.itemRequest} key={index} {...itemData} />
+    )
+  })
+
+  return (
+    <div className="list-div">
+    {Items}
+    </div>
+  )
+}
+
+export default ItemList
