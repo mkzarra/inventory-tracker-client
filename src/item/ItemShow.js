56de937e3f946909a8ce67bb655033137ff15910
mkzarra <mkzarra@gmail.com>
2018-10-18 11:58:43 -0400

    fixed render in ItemShow.js

--- src/item/ItemShow.js
+++ src/item/ItemShow.js
@@ -2,9 +2,8 @@ import React from 'react'
 import axios from 'axios'
 import { apiUrl } from '../server'
 import Nav from '../shared/Nav'
-import Item from './Item'
 
-class MovieIndex extends React.Component {
+export default class MovieIndex extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -23,9 +22,13 @@ class MovieIndex extends React.Component {
     return (
       <React.Fragment>
         <Nav />
-        <Item />
+        <h4 className="item-name">Item: {item.name}</h4>
+        <p className="item-storage">Storage: {item.storage}</p>
+        <p className="item-expiration">Expiration: {item.expiration}</p>
+        <p className="item-volume">Volume: {item.volume}</p>
+        <p className="item-unit">Unit: {item.unit}</p>
+        <p className="item-id">ID: {item._id}</p>
       </React.Fragment>
     )
   }
-
 }
