926c9a9c48447acded8079e30d711348e8215b91
mkzarra <mkzarra@gmail.com>
2019-01-22 14:53:02 -0500

    deleted unnecessary code

--- src/item/ItemIndex.js
+++ src/item/ItemIndex.js
@@ -20,56 +20,25 @@ class ItemIndex extends Component {
     this.setState({ name: e.target.value })
   }
 
-  handleSubmit = (e) => {
-    debugger
+  handleDelete = (e) => {
     e.preventDefault()
-    console.log(this.props.user.token)
-    axios.delete(`${apiUrl}/items/${e.target.value}`, { headers: { 'Authorization': 'Bearer ' + this.props.user.token } })
-      .then(res => {
-        console.log(res.data.items);
-        console.log(res.data);
-      })
+    const itemId = e.target.value
+    axios.delete(`${apiUrl}/items/${itemId}`, { headers: { 'Authorization': 'Bearer ' + this.props.user.token } })
+      .then(res => console.log(res.data))
+      .catch(err => console.error(err));
   }
-
-  deleteItemHandler = (e, data) => {
-    e.preventDefault();
-    console.log('[deleteItemHandler] in ItemIndex container', data);
-  }
-
-  // getItemIndex = e => {
-  //   e.preventDefault()
-
-  //   const { setItem } = this.props
-    
-  //   getItemIndex(this.state)
-  //     .then(handleErrors)
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       setItem(res.item)
-  //       console.log(res.item)
-  //     })
-  // }
 
   componentDidMount() {
     console.log('[componentDidMount] ItemIndex', this.props.user)
     axios.get(`${apiUrl}/items`)
       .then(res => {
         console.log(res.data.items)
-        this.setState({items: res.data.items})
+        this.setState({items: res.data.items.reverse()})
       })
   }
 
-  // deleteItem(itemId) {    
-  //   axios.delete(`${apiUrl}/items/${itemId}`)
-  //     .then(res => { 
-  //       console.log(res.data.items)
-  //       this.setState({items: res.data.items.map(item => itemId = item._id)})
-  //     })
-  // }
-
   render() {
-    const items = this.state.items
-    const renderItems = items.map(item => {
+    const renderItems = this.state.items.map(item => {
       return (
         <Item key={item._id}
           itemName={item.name}
@@ -78,28 +47,14 @@ class ItemIndex extends Component {
           volume={item.volume}
           unit={item.unit}
           id={item._id}
-          clicked={this.handleSubmit}
+          clicked={this.handleDelete}
         />
-
-      // <li key={item._id} className="card">
-      //   <div className="item-div">
-      //     <h3 className="item-name">Item: {item.name}</h3>
-      //     <p className="item-storage">Storage: {item.storage}</p>
-      //     <p className="item-expiration">Expiration: {item.expiration.slice(0,10)}</p>
-      //     <p className="item-volume">Volume: {item.volume}</p>
-      //     <p className="item-unit">Unit: {item.unit}</p>
-      //     <p className="item-id">ID: {item._id}</p>
-      //     <button className="remove" type="submit" name="remove" value={item._id} onChange={this.handleChange} onClick={this.handleSubmit}>Remove</button>
-      //     <button type="submit" className="update" value={item._id} onClick={this.props.onClick}>Update</button>
-      //     </div>
-      //     <br />
-      // </li>
       )
     })
 
     return (
       <React.Fragment>  
-        {renderItems}
+        {this.state.items.length > 0 ? renderItems : <h2>You currently have no items to display!</h2>}
       </React.Fragment>
     )
   }
